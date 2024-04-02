7897b99144f8e382773c8a1332b9f2179faa0973
mbullock710 <michael.bullock@guildeducation.com>
2024-04-01 21:22:01 -0500

    deleted commented out code from App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import './App.css'
 import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
-// import AboutMe from './components/AboutMe';
-// import Portfolio from './components/Portfolio';
-// import Contact from './components/Contact';
-// import Resume from './components/Resume';
 import Footer from './components/Footer';
 
 function App() {
@@ -23,28 +18,3 @@ function App() {
 }
 
 export default App;
-   
-   
-   
-
-
-
-
-    {/* <Router>
-      <div className="App">
-        <Header />
-        <Navigation />
-        <Routes>
-          <Route exact path="/" component={AboutMe} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/resume" component={Resume} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-    </div>
-  );
-}; */}
-
-
